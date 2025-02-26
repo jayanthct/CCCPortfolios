b383e14b40a4e6fa74b8e34c235434a5d3e26c13
Jayanth <jayanthct9999@gmail.com>
2025-02-26 15:08:33 +0530

    completed navbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
    <>
    <nav className="navbar flex justify-between px-[10%] py-[2%] items-center bg-[#49462326]">
-    <img src={logo} alt="" className="image w-[200px]" />
+    <img src={logo} alt="" className="image md:w-[200px] w-[100px]" />
     <button className='bg-[#494623] text-white font-bold flex justify-center items-center w-[120px] h-[52px] rounded-full'>Login</button>
    </nav>
    </>
